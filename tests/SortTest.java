a0b0d06219f739bdfd6cd200427670807e24c392
pabloozamora <pabloozamora02@gmail.com>
2022-03-01 20:09:51 -0600

    JUnit Tests
    Tests de JUnit para bubble sort, quicks ort y gnome sort.

---
+++ tests/SortTest.java
@@ -0,0 +1,51 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SortTest {
+
+	@Test
+	void testBubbleSort() {
+		Sort comparador = new Sort(new CompareInt());
+		Integer[] arreglo = new Integer[] {4,12,7,8,26,55,21}; //4,7,8,12,21,26,55
+		comparador.bubbleSort(arreglo);
+		assertEquals(arreglo[0], 4);
+		assertEquals(arreglo[1], 7);
+		assertEquals(arreglo[2], 8);
+		assertEquals(arreglo[3], 12);
+		assertEquals(arreglo[4], 21);
+		assertEquals(arreglo[5], 26);
+		assertEquals(arreglo[6], 55);
+	}
+
+	@Test
+	void testQuickSort() {
+		Sort comparador = new Sort(new CompareInt());
+		Integer[] arreglo = new Integer[] {4,12,7,8,26,55,21}; //4,7,8,12,21,26,55
+		comparador.quickSort(arreglo, 0, arreglo.length-1);
+		assertEquals(arreglo[0], 4);
+		assertEquals(arreglo[1], 7);
+		assertEquals(arreglo[2], 8);
+		assertEquals(arreglo[3], 12);
+		assertEquals(arreglo[4], 21);
+		assertEquals(arreglo[5], 26);
+		assertEquals(arreglo[6], 55);
+	}
+
+	@Test
+	void testGnomeSort() {
+		Sort comparador = new Sort(new CompareInt());
+		Integer[] arreglo = new Integer[] {4,12,7,8,26,55,21}; //4,7,8,12,21,26,55
+		comparador.gnomeSort(arreglo, arreglo.length);
+		assertEquals(arreglo[0], 4);
+		assertEquals(arreglo[1], 7);
+		assertEquals(arreglo[2], 8);
+		assertEquals(arreglo[3], 12);
+		assertEquals(arreglo[4], 21);
+		assertEquals(arreglo[5], 26);
+		assertEquals(arreglo[6], 55);
+	}
+
+}
+
