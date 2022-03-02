cafe9a1daf267d140f9187ce1553401dfd1c5250
pabloozamora <pabloozamora02@gmail.com>
2022-03-01 18:48:33 -0600

    Bubble Sort y Quicksort
    Implementados Bubble Sort y QuickSort sin probar

--- Sort.java
+++ Sort.java
@@ -4,4 +4,46 @@ public class Sort <T> {
 	public Sort(IComparator comparador) {
 		this.comparador = comparador;
 	}
+
+    public void bubbleSort(T[] myArray){
+        for (int i = 0; i<myArray.length-1; i++){
+            for (int j = 0; i < myArray.length; j++){
+                if(comparador.Compare(myArray[i], myArray[j]) > 0){
+                    T temp = myArray[i];
+                    myArray[i] = myArray[j];
+                    myArray[j] = temp;
+                }
+            }
+        }
+    }
+
+    public void quickSort(T[] myArray, int inf, int sup){
+        int i = inf - 1;
+        int j = sup;
+        boolean flag = true;
+        T temp;
+        if (inf >= sup){
+            return;
+        }
+
+        T element_div = myArray[sup];
+
+        while (flag){
+            while(comparador.Compare(myArray[++i], element_div) < 0);
+            while(comparador.Compare(myArray[--j], element_div) > 0 && (j>inf));
+            if (i < j){
+                temp = myArray[i];
+                myArray[i] = myArray[j];
+                myArray[j] = temp;
+            }
+            else{
+            flag = false;  
+            }
+        }
+        temp = myArray[i];
+	    myArray[i] = myArray[sup];
+	    myArray[sup] = temp;
+	    quickSort(myArray, inf, i - 1);
+	    quickSort(myArray, i + 1, sup);
+    }
 }
